f98c9186d5f3344b608470b360852cf728634de3
Sathyan Murugan <sathyan.murugan@outlook.com>
2017-08-19 23:52:23 +0200

    Implemented user login and session storage

--- app.py
+++ app.py
@@ -1,13 +1,53 @@
 import flask
+import util
 import os
+import spotipy.oauth2 as oauth2
+
 app = flask.Flask(__name__)
+app.debug = os.environ['DEBUG']
+app.secret_key = os.environ['APP_SECRET']
 
-
-
+#Initilize Auth Class
+auth = util.AuthUser()
 
 @app.route('/')
 def main():
-	return flask.render_template('main.html')
+	if 'token_data' in flask.session:
+		try:
+			#Check if token is expired, and if so, refresh
+			if auth.is_token_expired(flask.session['token_data']):
+				token_data = auth.refresh_token(flask.session['token_data'])
+				flask.session['token_data'] = token_data
+			else:
+				pass
+			return flask.redirect(flask.url_for('factory'))
+
+		except:
+			#in case user removed permissions for the app, the old tokens are no longer valid
+			#we need to reauthenticate
+			return flask.render_template('main.html')
+
+	else:
+		return flask.render_template('main.html')
+
+@app.route('/login')
+def login():
+	#Redirect user to Spotify for authentication	
+	return flask.redirect(auth.get_auth_url())
+
+
+@app.route('/usercheck')
+def usercheck():
+	"""authenticated user arrives here
+	"""
+	token_data = auth.get_token_data(flask.request.url)
+	flask.session['token_data'] = token_data
+	return flask.redirect(flask.url_for('factory'))
+
+@app.route('/factory')
+def factory():
+	return str(flask.session['token_data'])
+
 
 if __name__ == '__main__':
-    app.run(debug=os.environ['DEBUG'])
+    app.run()
